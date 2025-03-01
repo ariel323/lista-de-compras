e073fcf79f81353d49e376f0434e72cba115386a
Ariel Almada <arielalmada861@gmail.com>
2025-03-01 09:48:58 -0300

    versión 1.1

---
+++ app.js
@@ -0,0 +1,65 @@
+const listaDeCompras = {};
+
+// Función para agregar un alimento a la lista
+function agregarAlimento() {
+  let alimento = document.getElementById("alimento").value;
+  let categoria = document.getElementById("categoria").value.toLowerCase();
+
+  if (!listaDeCompras[categoria]) {
+    listaDeCompras[categoria] = [];
+  }
+
+  listaDeCompras[categoria].push(alimento);
+  mostrarLista();
+}
+
+// Función para mostrar la lista de compras organizada
+function mostrarLista() {
+  const listaElement = document.getElementById("lista");
+  listaElement.innerHTML = "";
+
+  for (let categoria in listaDeCompras) {
+    const categoriaElement = document.createElement("h3");
+    categoriaElement.innerText = categoria.toUpperCase();
+    listaElement.appendChild(categoriaElement);
+
+    const alimentosElement = document.createElement("ul");
+    listaDeCompras[categoria].forEach((alimento) => {
+      const alimentoElement = document.createElement("li");
+      alimentoElement.innerText = alimento;
+      alimentosElement.appendChild(alimentoElement);
+    });
+    listaElement.appendChild(alimentosElement);
+  }
+}
+
+// Función para eliminar un alimento de la lista
+function eliminarAlimento() {
+  let alimento = document.getElementById("alimento").value;
+  let categoria = document.getElementById("categoria").value.toLowerCase();
+
+  if (listaDeCompras[categoria]) {
+    let indice = listaDeCompras[categoria].indexOf(alimento);
+    if (indice !== -1) {
+      listaDeCompras[categoria].splice(indice, 1);
+      if (listaDeCompras[categoria].length === 0) {
+        delete listaDeCompras[categoria];
+      }
+      mostrarLista();
+    } else {
+      alert("El alimento no se encuentra en la categoría especificada.");
+    }
+  } else {
+    alert("La categoría especificada no existe.");
+  }
+}
+
+// Evento para el botón de agregar
+document
+  .getElementById("agregarBtn")
+  .addEventListener("click", agregarAlimento);
+
+// Evento para el botón de eliminar
+document
+  .getElementById("eliminarBtn")
+  .addEventListener("click", eliminarAlimento);
